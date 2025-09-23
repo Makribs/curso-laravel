a911e6f35871015467f95b89ce7695b282302a9f
Marcos Kauan Santos Ribas <markauansr@gmail.com>
2025-09-23 19:06:05 -0300

    Criei uma Flash Message para quando um evento for cadastrado com sucesso

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -15,10 +15,11 @@ Route::post('/events', [EventController::class, 'store']);
 Route::get('/events/update', [EventController::class, 'update']);
 Route::get('/events/delete', [EventController::class, 'delete']);
 
+/*
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/create', [ProductController::class, 'create']);
 Route::get('/products/read', [ProductController::class, 'read']);
 Route::get('/products/update', [ProductController::class, 'update']);
 Route::get('/products/delete', [ProductController::class, 'delete']);
-
+*/
 Route::get('/contacts', [ContactController::class, 'index']);
