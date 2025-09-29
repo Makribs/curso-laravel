3449912fdc1026e2e3454c1d090ef1e15d898222
Marcos Kauan Santos Ribas <markauansr@gmail.com>
2025-09-29 18:39:36 -0300

    Adicionando lógica de logout e botões para navegação com livewire

--- hdcevents/resources/views/layouts/main.blade.php
+++ hdcevents/resources/views/layouts/main.blade.php
@@ -30,12 +30,29 @@
                         <li class="nav-item">
                             <a href="/events/create" class="nav-link">Create Event</a>
                         </li>
+                        @auth
                         <li class="nav-item">
-                            <a href="/" class="nav-link">Login</a>
+                            <a href="/dashboard" class="nav-link">My Events</a>
                         </li>
                         <li class="nav-item">
-                            <a href="/" class="nav-link">Register</a>
+                            <form action="/logout" method="post">
+                                @csrf
+                                <a href="/logout" 
+                                    class="nav-link" 
+                                    onclick="event.preventDefault(); 
+                                    this.closest('form').submit();">
+                                Logout</a>
+                            </form>
                         </li>
+                        @endauth
+                        @guest
+                        <li class="nav-item">
+                            <a href="/login" class="nav-link">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/register" class="nav-link">Register</a>
+                        </li>
+                        @endguest
                     </ul>
                 </div>
             </nav>
